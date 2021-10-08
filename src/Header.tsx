da0e716e5039d46e9039e056f8f7f63bc0645a61
theakfl <theakfl@stud.ntnu.no>
2021-10-08 15:54:47 +0200

    #1 Add header and filter-box

---
+++ src/Header.tsx
@@ -0,0 +1,20 @@
+import { Flex, Heading, useColorModeValue } from "@chakra-ui/react";
+
+export const Header = () => {
+  const bgcolor = useColorModeValue("teal", "whiteAlpha.50");
+
+  return (
+    <Flex padding={6} bg={bgcolor} color="white" shadow="md">
+      <Flex align="center" mr={5}>
+        <Heading
+          as="h1"
+          size="md"
+          letterSpacing={"tighter"}
+          fontSize={{ base: "0px", sm: "0px", lg: "40px" }}
+        >
+          Pokémon
+        </Heading>
+      </Flex>
+    </Flex>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,13 @@ import { store } from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Header } from "./Header";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ChakraProvider>
+        <Header />
         <App />
       </ChakraProvider>
     </Provider>
@@ -18,7 +20,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
