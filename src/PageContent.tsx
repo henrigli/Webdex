bec17bf995f0605420d81c33603854b84427d020
theakfl <theakfl@stud.ntnu.no>
2021-10-14 13:53:29 +0200

    #8 Fix grid with Chakra

--- src/PageContent.tsx
+++ src/PageContent.tsx
@@ -1,17 +1,23 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
 import { SearchResults } from "./SearchResults";
 
 export const PageContent = () => {
   return (
-    <div className="wrapper">
-      <div className="filter">
-        <Box maxW="sm" borderWidth="1px" borderRadius="lg" px={3} h={400}>
-          Input
+    <Grid
+      h="200px"
+      templateRows="repeat(1, 1fr)"
+      templateColumns="repeat(2, 1fr)"
+      gap={4}
+    >
+      <GridItem colSpan={1}>
+        {/* <Filterbox /> */}
+        <Box maxW="sm" borderWidth="1px" borderRadius="lg">
+          Filter
         </Box>
-      </div>
-      <div className="searchResults">
+      </GridItem>
+      <GridItem colSpan={1}>
         <SearchResults />
-      </div>
-    </div>
+      </GridItem>
+    </Grid>
   );
 };
