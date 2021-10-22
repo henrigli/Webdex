b4a0de8ae8b22e4e06ac032f2da1b0bf6d7dc392
oivinder <oivinder@stud.ntnu.no>
2021-10-22 13:29:44 +0200

    #2 Add more comprehensive search query

--- src/SearchResults.tsx
+++ src/SearchResults.tsx
@@ -1,24 +1,18 @@
 import { Box, Grid, GridItem } from "@chakra-ui/react";
 import { useQuery } from "@apollo/client";
-import { client, SEARCH_QUERY } from "./services/graphql";
-
-const Loading = () => {
-  return <div>Loading...</div>
-};
-
-const Error = () => {
-  return <div>Error :'(</div>
-};
+import { client, Pokemon, SEARCH_QUERY } from "./services/graphql";
 
 export const SearchResults = () => {
-  const _filter = "chu";
-
   const { loading, error, data } = useQuery(SEARCH_QUERY, {
-    variables: {filter: _filter}
+    variables: {
+      filter: "chu",
+      skip: 0,
+      limit: 3,
+    }
   });
 
-  if (loading) return Loading();
-  if (error) return Error();
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
 
   return (
     <div>
@@ -27,16 +21,31 @@ export const SearchResults = () => {
         templateColumns="repeat(auto-fit, minmax(200px, 1fr)"
         gap={4}
       >
-        {data.pokemon_search.map((p: any) => {
-          return (
-            <GridItem colSpan={1}>
-              <Box maxW="sm" borderWidth="1px" borderRadius="lg">
-               {p.name}
-              </Box>
-            </GridItem>);
-        })}
-
+        {data.pokemon_search.map((p: Pokemon) => <PokemonCard pokemon={p} />)}
       </Grid>
     </div>
   );
+};
+
+const PokemonCard = (props: {pokemon: Pokemon}) => {
+  return (
+    <GridItem colSpan={1}>
+      <Box maxW="sm" borderWidth="1px" borderRadius="lg">
+      <p>#{props.pokemon.id}: {props.pokemon.name}</p>
+      <p>{props.pokemon.types.join(", ")}</p>
+      <p>Height: {props.pokemon.height} m &mdash; Weight : {props.pokemon.weight} kg</p>
+      <p><em>{props.pokemon.description || "This is a Pokémon!"}</em></p>
+      </Box>
+    </GridItem>
+  );
+}
+
+const Loading = () => {
+  return <div>Loading...</div>
+};
+
+const Error = (props: {error: any}) => {
+  console.log(props.error);
+
+  return <div>Error :'(</div>
 };
--- src/services/graphql.ts
+++ src/services/graphql.ts
@@ -1,19 +1,60 @@
 import {
     ApolloClient,
     InMemoryCache,
-    ApolloProvider,
-    useQuery,
     gql
   } from "@apollo/client";
   
-  export const client = new ApolloClient({
-      uri: 'http://localhost:4000/graphql',
-      cache: new InMemoryCache()
-    });
-  
-  export const SEARCH_QUERY = gql`
-  query Query($filter: String){
-    pokemon_search(filter: $filter){
+export interface Pokemon {
+  id: number;
+  name: String;
+  types: String[];
+  height: number;
+  weight: number,
+  description: String;
+};
+
+// http://it2810-06.idi.ntnu.no/
+export const client = new ApolloClient({
+    uri: 'http://it2810-06.idi.ntnu.no/graphql',
+    cache: new InMemoryCache()
+  });
+
+export const FIND_ONE = gql`
+  query Query($id: Int) {
+    pokemon(id: $id) {
+      id
+      name
+      types
+      height
+      weight
+      description
+    }
+  }
+`;
+
+export const SEARCH_QUERY = gql`
+  query Query(
+    $filter: String,
+    $type: String,
+    $height_lte: Int,
+    $height_gte: Int,
+    $weight_lte: Int,
+    $weight_gte: Int,
+    $skip: Int,
+    $limit: Int,
+    $sort: String,
+    ){
+    pokemon_search(
+      filter: $filter,
+      type: $type,
+      height_lte: $height_lte,
+      height_gte: $height_gte,
+      weight_lte: $weight_lte,
+      weight_gte: $weight_gte,
+      skip: $skip,
+      limit: $limit,
+      sort: $sort,
+      ){
       id
       name
       types
@@ -22,4 +63,4 @@ import {
       description
     }
   }  
-  `;
+`;
