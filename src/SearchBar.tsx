774016647b14fb8b0f00e10d7cc643fce34f704c
henrigli <henrigli@stud.ntnu.no>
2021-10-26 16:33:16 +0200

    Minor changes to searchbar

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -3,8 +3,7 @@ import {
     Button,
     FormControl,
     FormLabel,
-    Input,
-    Box
+    Input
   } from "@chakra-ui/react"
 
 export const SearchBar = () => {
@@ -12,9 +11,8 @@ export const SearchBar = () => {
     console.log(query)
     return (
         <FormControl class="searchField" action="/" method="get">
-            <FormLabel className="label" htmlFor="query">Søk etter Pokémon!</FormLabel>
-            <Box> </Box>
-            <Input type="text" name="query" placeholder={"Søk etter Pokémon!"} value= {query} onChange={(e) => setQuery(e.target.value)} />
+            <FormLabel className="label" htmlFor="query"></FormLabel>
+            <Input type="text" name="query" placeholder={"Søk etter Pokémon!"} value={query} onChange={(e) => setQuery(e.target.value)} />
             <Button size="md" colorScheme="teal" w="full" type="submit"> Submit </Button>
         </FormControl>
   );
