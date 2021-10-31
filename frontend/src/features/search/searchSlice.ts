54662d0b42895e498b2effa37310f9b6fdc48f5f
oivinder <oivinder@stud.ntnu.no>
2021-10-31 22:02:06 +0100

    #21 Implement pagination

--- frontend/src/features/search/searchSlice.ts
+++ frontend/src/features/search/searchSlice.ts
@@ -2,15 +2,19 @@ import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const initialState: {
-  filter: String;
-  minWeight: Number;
-  maxWeight: Number;
-  sort: String;
+  filter: string;
+  minWeight: number;
+  maxWeight: number;
+  sort: string;
+  skip: number;
+  limit: number;
 } = {
   filter: "",
   minWeight: 0,
   maxWeight: 0,
   sort: "",
+  skip: 0,
+  limit: 24,
 };
 
 export const searchSlice = createSlice({
@@ -41,5 +45,23 @@ export const searchSlice = createSlice({
     clearSort: (state) => {
       state.sort = "";
     },
+    setSkip: (state, action: PayloadAction<number>) => {
+      state.skip = action.payload;
+    },
+    clearSkip: (state) => {
+      state.skip = 0;
+    },
+    setLimit: (state, action: PayloadAction<number>) => {
+      state.limit = action.payload;
+    },
+    clearLimit: (state) => {
+      state.limit = 24;
+    },
+    prevPage: (state) => {
+      state.skip = Math.max(state.skip - state.limit, 0);
+    },
+    nextPage: (state) => {
+      state.skip = state.skip + state.limit;
+    }
   },
 });
