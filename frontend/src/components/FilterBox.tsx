618040e2a9ea18a51312248138742f6bf9f923de
Henrik Giil Liisberg <henrigli@stud.ntnu.no>
2021-11-18 10:54:53 +0100

    Updated icons and removed filterbox border

--- frontend/src/components/FilterBox.tsx
+++ frontend/src/components/FilterBox.tsx
@@ -2,18 +2,10 @@ import { Container, Flex, Heading } from "@chakra-ui/react";
 import { WeightSlider } from "./WeightSlider";
 //import { TypeCheckboxes } from "./TypeCheckboxes";
 import { SearchBar } from "./SearchBar";
-import { SortDropdownMenu } from "./SortDropdownMenu";
 
 export const FilterBox = () => {
   return (
-    <Flex
-      p={4}
-      pos="sticky"
-      w="100%"
-      borderWidth="1px"
-      borderRadius="lg"
-      overflow="hidden"
-    >
+    <Flex p={4} pos="sticky" w="100%" overflow="hidden">
       <Container>
         <Heading align="left">Filter</Heading>
         <WeightSlider />
